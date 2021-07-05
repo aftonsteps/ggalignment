2f7c27123dff2741ed8aa2fb711b9a44c0ff73ef
Tan-DESKPC <tan@tanho.ca>
2021-07-05 10:55:41 -0400

    simplify usage of system.file a bit

--- R/align_cats.R
+++ R/align_cats.R
@@ -8,13 +8,14 @@
 #' @examples
 #' align_cats()
 align_cats <- function() {
-  oberon <- paste0(system.file("ggalignment"), "inst/img/obie.png")
-  jerry <- paste0(system.file("ggalignment"), "inst/img/jerry.png")
-  gray <- paste0(system.file("ggalignment"), "inst/img/gray_3.png")
-  jeff <- paste0(system.file("ggalignment"), "inst/img/jeff_2.png")
+
+  cats <- system.file(c("img/obie.png",
+                        "img/jerry.png",
+                        "img/gray.png",
+                        "img/jeff.png"))
 
   align_cats <-
-    data.frame(img = c(oberon, jeff, jerry, gray),
+    data.frame(img = cats,
                alignment = c("chaotic evil",
                              "lawful neutral",
                              "chaotic good",
