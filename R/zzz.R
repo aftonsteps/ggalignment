346475396fa0cb22b6ebc4f78131928b31d7d080
aftonsteps <aftoncoombs@gmail.com>
2021-09-03 20:55:20 -0700

    Fixed globals CRAN warning
    - Added globals.R
    - Used .data within ggplot calls

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,7 @@
 .onLoad <- function(libname,pkgname){
   align_cats <<- align_cats()
 }
+
+# assign(x = "align_cats",
+#        value = ggalignment::align_cats(),
+#        envir = parent.env(environment()))
